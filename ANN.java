212771c4690db9a984bd357f37fcf9acf8c8635a
Hermann Ritter <h.ritter1992@gmail.com>
2016-11-24 22:51:20 +0100

    Add files via upload
    Bugfixing.

--- ANN.java
+++ ANN.java
@@ -3,19 +3,19 @@ public class ANN {
 		double[] weights = new double[3]; //saves the weights into an array, every index is another node
 		double[] inputs = new double[3];  //saves the inputs into an array
 		double output = 0; 				 //output of the output node
-		double learningRate = 0.1;		//learning rate of the learning algorithm
+		double learningRate = 0.01;		//learning rate of the learning algorithm
 		double goal = 0;				//goal is telling the supervised learning algorithm which output is correct
 		
 		inputs[0] = 1; //index 0 is the bias node, input there is always 1
-		for (double weight : weights){
-			weight = Math.random()-1; //sets the weights to a random negative value
+		for (int i = 0; i < weights.length; i++){
+			weights[i] = Math.random()-1; //sets the weights to a random negative value
 		}
 		
 		//Does 500 iterations of the learning (overkill for the OR-Gate since the weights converge quite quickly)
 		for (int iteration = 1; iteration <= 500; iteration++) {
 			//creates a random number between 0.5 and 1.5, then get's rid of the stuff after the decimal point
-			inputs[1] = Math.floor(Math.random()+0.5); 
-			inputs[2] = Math.floor(Math.random()+0.5);
+			inputs[1] =Math.floor(Math.random()+0.5); 
+			inputs[2] =Math.floor(Math.random()+0.5);
 			
 			goal = 1; //sets the goal to one, changes it if both inputs are 0 to 0 (OR-Gate)
 			if (inputs[1] == 0 && inputs[2] == 0)
