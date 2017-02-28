f33c7d1abfadb3e11871f901873676a5664484de
Hermann Ritter <h.ritter1992@gmail.com>
2017-03-01 00:43:38 +0100

    Add files via upload

--- Perceptron.java
+++ Perceptron.java
@@ -7,24 +7,24 @@ public class Perceptron {
 		double[] outputState = new double[outputs.length];
 		double learningRate = 0.1;
 				
-		for (int i = 0; i < weights.length; i++) {
+		for (int i = 0; i < weights.length; i++) 
 			weights[i] = Math.random()-Math.random();
-		}
+
 		inputs[inputs.length-1] = 1;
 		
 		for (int iterations = 1; iterations < 100; iterations++) {
-			for (int inputGenerator = 0; inputGenerator < inputs.length-1; inputGenerator++) {
+			for (int inputGenerator = 0; inputGenerator < inputs.length-1; inputGenerator++) 
 				inputs[inputGenerator] = (double) Math.floor(Math.random()+0.5);
-			}
 			
 			System.out.println("Weights InOne: " +weights[0]);
 			System.out.println("Weights InTwo: " +weights[1]);
 			System.out.println("Weights Bias: " +weights[2]);
 			System.out.println("Input 1: " +inputs[0]);
 			System.out.println("Input 2: " +inputs[1]);
+			
 			for (int outputIterator = 0; outputIterator < outputState.length; outputIterator++) {
 				for (int inputIterator = 0; inputIterator < inputs.length; inputIterator++)
-						outputState[outputIterator] += weights[inputIterator] * inputs[inputIterator];
+					outputState[outputIterator] += weights[inputIterator] * inputs[inputIterator];
 				
 				outputs[outputIterator] = activation(outputState[outputIterator]);
 				outputState[outputIterator] = 0;
@@ -41,9 +41,6 @@ public class Perceptron {
 	}
 	
 	public static double activation(double state) {
-		if (state >= 0)
-			return 1;
-		else
-			return 0;
+		return (state >= 0) ? 1 : 0;
 	}
 }
